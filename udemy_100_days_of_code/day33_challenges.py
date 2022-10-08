25ed0063646ea576c02c56eb009a96e9ecd22ba2
David Betz <hashref@gmail.com>
2022-10-07 17:35:16 -0700

    Finish udemy 100 days of code, day 33 first challenge
    Signed-off-by: David Betz <hashref@gmail.com>

--- udemy_100_days_of_code/day33_challenges.py
+++ udemy_100_days_of_code/day33_challenges.py
@@ -1,5 +1,31 @@
+from typing import Literal, TypedDict
+import requests
+import datetime as dt
+
+
+class ISS_POSITION_RESPONSE(TypedDict):
+    message: Literal["success"]
+    timestamp: float
+    iss_position: dict[Literal["longitude", "latitude"], float]
+
+
+def iss_position() -> tuple[dt.datetime, float, float]:
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    response_data: ISS_POSITION_RESPONSE = response.json()
+
+    position_data = response_data["iss_position"]
+    current_position = (
+        dt.datetime.fromtimestamp(response_data["timestamp"]),
+        position_data["latitude"],
+        position_data["longitude"],
+    )
+
+    return current_position
+
+
 def main() -> None:
-    pass
+    print(iss_position())
 
 
 if __name__ == "__main__":
