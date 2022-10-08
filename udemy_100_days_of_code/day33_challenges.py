0f52eca797e7d4fc92101332946557d2e39e3d4a
David Betz <hashref@gmail.com>
2022-10-07 21:53:10 -0700

    Finish 100 days of code, day 33 last challenge
    Signed-off-by: David Betz <hashref@gmail.com>

--- udemy_100_days_of_code/day33_challenges.py
+++ udemy_100_days_of_code/day33_challenges.py
@@ -1,13 +1,24 @@
 import datetime as dt
+import smtplib
+import time
+from datetime import timezone
 from tkinter import Button, Canvas, PhotoImage, Tk
 from typing import Literal, TypedDict
+from signal import signal, SIGINT
 
 import requests
+from config42 import ConfigManager
+from config42.handlers import FileHandler
 from menutools import Menu
 
 ASSETS_DIRECTORY = "udemy_100_days_of_code/day33_assets"
 LATITUDE = 34.052235
 LONGITUDE = -118.243683
+
+
+CONFIG = ConfigManager(
+    handler=FileHandler, path="udemy_100_days_of_code/config.json"
+).as_dict()
 
 
 class ISS_POSITION_RESPONSE(TypedDict):
@@ -39,16 +50,18 @@ class SUNRISE_SUNSET_API_RESPONSE(TypedDict):
     results: dict[SUNRISE_SUNSET_RESULTS_KEYS, str]
 
 
-def iss_position() -> tuple[dt.datetime, float, float]:
+def get_iss_position() -> tuple[dt.datetime, float, float]:
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     response_data: ISS_POSITION_RESPONSE = response.json()
 
     position_data = response_data["iss_position"]
     current_position = (
-        dt.datetime.fromtimestamp(response_data["timestamp"]),
-        position_data["latitude"],
-        position_data["longitude"],
+        dt.datetime.fromtimestamp(response_data["timestamp"]).astimezone(
+            tz=timezone.utc
+        ),
+        float(position_data["latitude"]),
+        float(position_data["longitude"]),
     )
 
     return current_position
@@ -86,7 +99,7 @@ def kanye_says() -> None:
     window.mainloop()
 
 
-def sunrise_sunset() -> tuple[str, str]:
+def get_sunrise_sunset() -> tuple[dt.datetime, dt.datetime]:
     payload = {
         "lat": LATITUDE,
         "lng": LONGITUDE,
@@ -97,15 +110,84 @@ def sunrise_sunset() -> tuple[str, str]:
 
     data: SUNRISE_SUNSET_API_RESPONSE = response.json()
 
-    return (data["results"]["sunrise"], data["results"]["sunset"])
+    sunrise = dt.datetime.fromisoformat(data["results"]["sunrise"])
+    sunset = dt.datetime.fromisoformat(data["results"]["sunset"])
+
+    return (sunrise, sunset)
+
+
+def is_dark(now: dt.datetime) -> bool:
+    sunrise_sunset_data = get_sunrise_sunset()
+    sunrise = sunrise_sunset_data[0]
+    sunset = sunrise_sunset_data[1]
+
+    if now > sunset or now < sunrise:
+        return True
+
+    return False
+
+
+def is_iss_overhead() -> bool:
+    iss_position = get_iss_position()
+    iss_latitude = iss_position[1]
+    iss_longitude = iss_position[2]
+
+    delta_latitude = abs(LATITUDE - iss_latitude)
+    delta_longitude = abs(LONGITUDE - iss_longitude)
+
+    if int(delta_latitude) <= 5 and int(delta_longitude) <= 5:
+        return True
+    else:
+        print(f"Current ISS Delta: {delta_latitude} lat, {delta_longitude} lng")
+
+    return False
+
+
+def iss_tracker() -> None:
+    running = True
+
+    def handler(sig, frame) -> None:
+        print("Process interrupted. Exiting!")
+        main()
+
+    signal(SIGINT, handler)
+
+    while running:
+        current_time = dt.datetime.now().astimezone(tz=timezone.utc)
+
+        if is_dark(now=current_time):
+            print("Checking if the ISS is overhead")
+            if is_iss_overhead():
+                send_mail(
+                    subject="Look up!",
+                    body="Look up! The ISS is currently overhead!",
+                )
+
+                running = False
+
+        else:
+            running = False
+
+        time.sleep(60)
+
+
+def send_mail(subject: str, body: str) -> None:
+    with smtplib.SMTP(CONFIG["smtp_host"]) as connection:
+        connection.starttls()
+        connection.login(user=CONFIG["smtp_username"], password=CONFIG["smtp_password"])
+        connection.sendmail(
+            from_addr=CONFIG["email"]["from"],
+            to_addrs=CONFIG["email"]["to"],
+            msg=f"Subject:{subject}\n\n{body}",
+        )
 
 
 def main() -> None:
     def print_iss_position() -> None:
-        print(iss_position())
+        print(get_iss_position())
 
     def print_sunrise_sunset() -> None:
-        print(sunrise_sunset())
+        print(get_sunrise_sunset())
 
     menu = Menu("100 Days of Code Challenges")
     menu.add(
@@ -115,6 +197,7 @@ def main() -> None:
                 ("ISS Position", print_iss_position),
                 ("Kanye Says", kanye_says),
                 ("Sunrise/Sunset", print_sunrise_sunset),
+                ("ISS Tracker", iss_tracker),
                 ("Exit", menu.exit),
             ],
         )
